9ab1db02602eb753a0b0ab20710d3b83df570a64
aresyuan <s19940422@gmail.com>
2015-10-12 20:10:26 +0800

    add the javascript file index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+//id=right1
+document.getElementById("right1").addEventListener("mouseover", mouseOver);
+document.getElementById("right1").addEventListener("mouseout", mouseOut);
+						
+function mouseOver() {
+	document.getElementById("right1").style.paddingLeft = "30px";
+}	
+function mouseOut() {
+	document.getElementById("right1").style.paddingLeft = "0px";
+}
+//id=right2
+document.getElementById("right2").addEventListener("mouseover", mouseOver1);
+document.getElementById("right2").addEventListener("mouseout", mouseOut1);
+						
+function mouseOver1() {
+	document.getElementById("right2").style.paddingLeft = "30px";
+}	
+function mouseOut1() {
+	document.getElementById("right2").style.paddingLeft = "0px";
+}
+//id=right3
+document.getElementById("right3").addEventListener("mouseover", mouseOver2);
+document.getElementById("right3").addEventListener("mouseout", mouseOut2);
+						
+function mouseOver2() {
+	document.getElementById("right3").style.paddingLeft = "30px";
+}	
+function mouseOut2() {
+	document.getElementById("right3").style.paddingLeft = "0px";
+}
+//id=right4
+document.getElementById("right4").addEventListener("mouseover", mouseOver3);
+document.getElementById("right4").addEventListener("mouseout", mouseOut3);
+						
+function mouseOver3() {
+	document.getElementById("right4").style.paddingLeft = "30px";
+}	
+function mouseOut3() {
+	document.getElementById("right4").style.paddingLeft = "0px";
+}
+//id=right5
+document.getElementById("right5").addEventListener("mouseover", mouseOver4);
+document.getElementById("right5").addEventListener("mouseout", mouseOut4);
+						
+function mouseOver4() {
+	document.getElementById("right5").style.paddingLeft = "30px";
+}	
+function mouseOut4() {
+	document.getElementById("right5").style.paddingLeft = "0px";
+}
+//id=right6
+document.getElementById("right6").addEventListener("mouseover", mouseOver5);
+document.getElementById("right6").addEventListener("mouseout", mouseOut5);
+						
+function mouseOver5() {
+	document.getElementById("right6").style.paddingLeft = "30px";
+}	
+function mouseOut5() {
+	document.getElementById("right6").style.paddingLeft = "0px";
+}
+//id=right7
+document.getElementById("right7").addEventListener("mouseover", mouseOver6);
+document.getElementById("right7").addEventListener("mouseout", mouseOut6);
+						
+function mouseOver6() {
+	document.getElementById("right7").style.paddingLeft = "30px";
+}	
+function mouseOut6() {
+	document.getElementById("right7").style.paddingLeft = "0px";
+}
